35051d660d8925bb08efd77278f9539703922556
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-05-06 04:24:27 +0400

    fixed

--- src/components/FourthScreen/index.tsx
+++ src/components/FourthScreen/index.tsx
@@ -1,6 +1,8 @@
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import styles from './style.module.scss';
 import { useEffect, useRef, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import Typewriter from '../TypeWriter';
 
 function polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
     const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
@@ -15,90 +17,189 @@ function describeArc(x: number, y: number, radius: number, startAngle: number, e
     const end = polarToCartesian(x, y, radius, startAngle);
     const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
     const d = [
-        "M", start.x, start.y,
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+        "M", end.x, end.y,
+        "A", radius, radius, 0, largeArcFlag, 1, start.x, start.y
     ].join(" ");
     return d;
 }
 
 export const FourthScreen = () => {
-    const segmentVariants = {
-        hidden: { pathLength: 0, opacity: 0 },
-        visible: (i: number) => ({
-            pathLength: 1,
-            opacity: 1,
-            transition: { delay: i * 0.5, duration: 1 }
-        })
-    };
-
-    const [segments, setSegments] = useState({
-        Orange: { color: '#C3A46550', rotation: -50, degrees: 270 },
-        Red: { color: '#BF777B50', rotation: 221.5, degrees: 5 },
-        Yellow: { color: '#E3E1D950', rotation: 228, degrees: 38.5 },
-        Green: { color: '#A2B79550', rotation: 268, degrees: 40.5 },
-    })
 
     const strokeWidth = 108;
     const radius = 240;
 
     const refMain = useRef(null);
-    useEffect(() => {
-        const handleScroll = () => {
-            if (refMain.current) {
-                const bounding = (refMain.current as any).getBoundingClientRect()
-                const innerHeight = (refMain.current as any).offsetHeight;
-                const top = -(bounding.top - window.innerHeight)
 
+    const [refFirstBlock1, inViewFirstBlock1] = useInView({
+        triggerOnce: false,
+        threshold: 1,
+    });
+    const [refFirstBlock2, inViewFirstBlock2] = useInView({
+        triggerOnce: false,
+        threshold: 1,
+    });
+    const [refFirstBlock3, inViewFirstBlock3] = useInView({
+        triggerOnce: false,
+        threshold: 1,
+    });
+    const [refFirstBlock4, inViewFirstBlock4] = useInView({
+        triggerOnce: false,
+        threshold: 1,
+    });
 
-                let newSegments = { ...segments };
-                if (top > innerHeight * 0.3 && top < innerHeight * 0.78) {
-                    const dif = innerHeight * 0.78 - innerHeight * 0.3;
-                    newSegments = { ...newSegments, Orange: { color: '#C3A46550', rotation: -50, degrees: 270 / dif * (top - (innerHeight * 0.3)) } }
-                } else if (top < innerHeight * 0.3) {
-                    newSegments = { ...newSegments, Orange: { color: '#C3A46550', rotation: -50, degrees: 0 } }
-                } else {
-                    newSegments = { ...newSegments, Orange: { color: '#C3A46550', rotation: -50, degrees: 270 } }
-                }
+    const blockControls1 = useAnimation();
+    const blockControls2 = useAnimation();
+    const blockControls3 = useAnimation();
+    const blockControls4 = useAnimation();
 
-                if (top > innerHeight * 0.78 && top < innerHeight * 0.84) {
-                    const dif = innerHeight * 0.84 - innerHeight * 0.78;
-                    newSegments = { ...newSegments, Red: { color: '#BF777B50', rotation: 221.5, degrees: 5 / dif * (top - (innerHeight * 0.78)) } }
-                } else if (top < innerHeight * 0.78) {
-                    newSegments = { ...newSegments, Red: { color: '#BF777B50', rotation: 221.5, degrees: 0 } }
-                } else {
-                    newSegments = { ...newSegments, Red: { color: '#BF777B50', rotation: 221.5, degrees: 5 } }
-                }
+    const [percent1, title1, line1, dot1] = [useAnimation(), useAnimation(), useAnimation(), useAnimation()];
+    const [percent2, title2, line2, dot2] = [useAnimation(), useAnimation(), useAnimation(), useAnimation()];
+    const [percent3, title3, line3, dot3] = [useAnimation(), useAnimation(), useAnimation(), useAnimation()];
+    const [percent4, title4, line4, dot4] = [useAnimation(), useAnimation(), useAnimation(), useAnimation()];
 
-                if (top > innerHeight * 0.84 && top < innerHeight * 0.92) {
-                    const dif = innerHeight * 0.92 - innerHeight * 0.84;
-                    newSegments = { ...newSegments, Yellow: { color: '#E3E1D950', rotation: 228, degrees: 38.5 / dif * (top - (innerHeight * 0.84)) } }
-                } else if (top < innerHeight * 0.84) {
-                    newSegments = { ...newSegments, Yellow: { color: '#E3E1D950', rotation: 228, degrees: 0 } }
-                } else {
-                    newSegments = { ...newSegments, Yellow: { color: '#E3E1D950', rotation: 228, degrees: 38.5 } }
-                }
-
-                if (top > innerHeight * 0.92 && top < innerHeight * 1) {
-                    const dif = innerHeight * 1 - innerHeight * 0.92;
-                    newSegments = { ...newSegments, Green: { color: '#A2B79550', rotation: 268, degrees: 40.5 / dif * (top - (innerHeight * 0.92)) } }
-                } else if (top < innerHeight * 0.92) {
-                    newSegments = { ...newSegments, Green: { color: '#A2B79550', rotation: 268, degrees: 0 } }
-                } else {
-                    newSegments = { ...newSegments, Green: { color: '#A2B79550', rotation: 268, degrees: 40.5 } }
-                }
-                setSegments(newSegments)
+    const [animations, setAnimations] = useState(
+        {
+            first: {
+                percent: percent1,
+                title: title1,
+                line: line1,
+                dot: dot1,
+                isValue: false
+            },
+            second: {
+                percent: percent2,
+                title: title2,
+                line: line2,
+                dot: dot2,
+                isValue: false
+            },
+            third: {
+                percent: percent3,
+                title: title3,
+                line: line3,
+                dot: dot3,
+                isValue: false
+            },
+            fourth: {
+                percent: percent4,
+                title: title4,
+                line: line4,
+                dot: dot4,
+                isValue: false
             }
-        };
+        }
+    )
 
+    const startAnimation = (control: any, animation: any, name: 'first' | 'second' | 'third' | 'fourth') => {
+        control.animate.start({
+            pathLength: 1,
+            transition: { duration: 2 }
+        })
 
-        window.addEventListener('scroll', handleScroll);
+        animation.percent.start('visible')
+        animation.title.start('visible')
+        animation.line.start('visible')
+        animation.dot.start('visible')
 
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+    }
+    const stopAnimation = (control: any, animation: any, name: 'first' | 'second' | 'third' | 'fourth') => {
+        control.animate.start({
+            pathLength: 0,
+            transition: { duration: 2 }
+        })
+
+        animation.percent.start('hidden')
+        animation.title.start('hidden')
+        animation.line.start('hidden')
+        animation.dot.start('hidden')
+        setAnimations({ ...animations, [name]: { ...animations[name], isValue: false } })
+    }
+
+    useEffect(() => {
+        const newAnimations = { ...animations };
+        if (inViewFirstBlock1) {
+            startAnimation(segments.Orange, animations.first, 'first')
+            newAnimations.first.isValue = true;
+        } else {
+            stopAnimation(segments.Orange, animations.first, 'first')
+            newAnimations.first.isValue = false;
+        }
+
+        if (inViewFirstBlock2) {
+            startAnimation(segments.Green, animations.fourth, 'fourth')
+            newAnimations.fourth.isValue = true;
+        } else {
+            stopAnimation(segments.Green, animations.fourth, 'fourth')
+            newAnimations.fourth.isValue = false;
+        }
+
+        if (inViewFirstBlock3) {
+            startAnimation(segments.Yellow, animations.third, 'third')
+            newAnimations.third.isValue = true;
+        } else {
+            stopAnimation(segments.Yellow, animations.third, 'third')
+            newAnimations.third.isValue = false;
+        }
+        if (inViewFirstBlock4) {
+            startAnimation(segments.Red, animations.second, 'second')
+            newAnimations.second.isValue = true;
+        } else {
+            stopAnimation(segments.Red, animations.second, 'second')
+            newAnimations.second.isValue = false;
+        }
+        setAnimations(newAnimations)
+    }, [inViewFirstBlock1, inViewFirstBlock2, inViewFirstBlock3, inViewFirstBlock4])
+
+    const [segments] = useState({
+        Orange: {
+            color: '#C3A46550',
+            rotation: -50,
+            degrees: 270,
+            percent: '80%',
+            lineWidth: 800,
+            title: 'Ecosystem',
+            value: '2 000 000 000',
+            animate: blockControls1,
+            ref: refFirstBlock1,
+        },
+        Green: {
+            color: '#BF777B50',
+            rotation: 221.5,
+            degrees: 5,
+            percent: '10%',
+            lineWidth: 650,
+            title: 'Presale',
+            value: '250 000 000',
+            animate: blockControls4,
+            ref: refFirstBlock4,
+        },
+        Yellow: {
+            color: '#E3E1D950',
+            rotation: 228,
+            degrees: 38.5,
+            percent: '9%',
+            lineWidth: 650,
+            title: 'Liquidity',
+            value: '225 000 000',
+            animate: blockControls3,
+            ref: refFirstBlock3,
+        },
+        Red: {
+            color: '#A2B79550',
+            rotation: 268,
+            degrees: 40.5,
+            percent: '1%',
+            lineWidth: 690,
+            title: 'Airdrop',
+            value: '25 000 000',
+            animate: blockControls2,
+            ref: refFirstBlock2,
+        },
+    })
 
     return (
         <div className={styles.container} ref={refMain}>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', position: 'absolute', left: 0, top: 0, width: '100%' }}>
                 <motion.svg width="595" height="595" viewBox="0 0 595 595" >
                     {Object.values(segments).map((segment, index) => (
                         <motion.path
@@ -107,13 +208,79 @@ export const FourthScreen = () => {
                             strokeWidth={strokeWidth}
                             stroke={segment.color}
                             d={describeArc(297.5, 297.5, radius, segment.rotation, segment.rotation + segment.degrees)}
-                            variants={segmentVariants}
-                            initial="hidden"
-                            animate="visible"
+                            initial={{ pathLength: 0 }}
+                            animate={segment.animate}
                             custom={index}
                         />
                     ))}
                 </motion.svg>
+            </div>
+            <div className={styles.infoBlock}>
+                <div className={styles.titleBlock}>
+                    TOKENOMICS
+                </div>
+                <div className={styles.content}>
+                    {Object.values(segments).map((segment, index) => (
+                        <div key={index} className={styles.eachBlock} ref={segment.ref}>
+                            <div className={styles.info}>
+                                <motion.div className={styles.percent}
+                                    animate={Object.values(animations)[index].percent}
+                                    initial="hidden"
+                                    variants={{
+                                        hidden: { opacity: 0, fontSize: '0px', marginRight: '0px' },
+                                        visible: {
+                                            opacity: 1,
+                                            fontSize: '28px',
+                                            marginRight: '10px',
+                                            transition: { duration: 0.5, delay: 1.1 }
+                                        }
+                                    }}
+                                >{segment.percent}</motion.div>
+                                <motion.div
+                                    animate={Object.values(animations)[index].title}
+                                    initial="hidden"
+                                    variants={{
+                                        hidden: { opacity: 0 },
+                                        visible: {
+                                            opacity: 1,
+                                            transition: { duration: 0.5, delay: 0.3 }
+                                        }
+                                    }}
+                                    className={styles.title}
+                                >{segment.title}</motion.div>
+                            </div>
+                            <div className={styles.lineBlock}>
+                                <motion.div
+                                    animate={Object.values(animations)[index].line}
+                                    initial="hidden"
+                                    variants={{
+                                        hidden: { width: 0 },
+                                        visible: {
+                                            width: segment.lineWidth,
+                                            transition: { duration: 0.5, delay: 0.5 }
+                                        }
+                                    }}
+                                    className={styles.line} style={{ width: segment.lineWidth }} />
+                                <motion.div className={styles.dot}
+                                    animate={Object.values(animations)[index].dot}
+                                    initial="hidden"
+                                    variants={{
+                                        hidden: { width: 0, height: 0 },
+                                        visible: {
+                                            width: 8, height: 8,
+                                            transition: { duration: 0.1, delay: 1 }
+                                        }
+                                    }}
+                                />
+                            </div>
+                            <div className={styles.value}>
+                                {Object.values(animations)[index].isValue &&
+                                    <Typewriter text={segment.value} classname="value" time={500} delay={500} />
+                                }
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/constants/screens.tsx
+++ src/constants/screens.tsx
@@ -6,10 +6,10 @@ import { SixthScreen } from "../components/SixthScreen";
 import { ThirdScreen } from "../components/ThirdScreen";
 
 export const screens = [
-  { id: 'MainScreen', color: 'blue', name: 'Home', component: <MainScreen />, imageUrl: '/bg-images/main-bg.png' },
-  { id: 'SecondScreen', color: 'black', name: 'Trailer', component: <SecondScreen /> , imageUrl: '/bg-images/second-bg.png'},
-  { id: 'ThirdScreen', color: 'green', name: 'How it works', component: <ThirdScreen />, imageUrl: '/bg-images/third-bg.png' },
-  { id: 'FourthScreen', color: 'yellow', name: 'Tokenomics', component: <FourthScreen />, imageUrl: '/bg-images/fourth-bg.png' },
-  { id: 'FifthScreen', color: 'gray', name: 'Roadmap', component: <FifthScreen />, imageUrl: '/bg-images/fifth-bg.png' },
-  { id: 'SixthScreen', color: 'red', name: 'FAQ', component: <SixthScreen />, imageUrl: '/bg-images/sixth-bg.png' },
+  { id: 'home', color: 'blue', name: 'Home', component: <MainScreen />, imageUrl: '/bg-images/main-bg.png' },
+  { id: 'trailer', color: 'black', name: 'Trailer', component: <SecondScreen /> , imageUrl: '/bg-images/second-bg.png'},
+  { id: 'how-it-works', color: 'green', name: 'How it works', component: <ThirdScreen />, imageUrl: '/bg-images/third-bg.png' },
+  { id: 'tokenomics', color: 'yellow', name: 'Tokenomics', component: <FourthScreen />, imageUrl: '/bg-images/fourth-bg.png' },
+  { id: 'roadmap', color: 'gray', name: 'Roadmap', component: <FifthScreen />, imageUrl: '/bg-images/fifth-bg.png' },
+  { id: 'faq', color: 'red', name: 'FAQ', component: <SixthScreen />, imageUrl: '/bg-images/sixth-bg.png' },
 ]
