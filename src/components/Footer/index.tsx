ecd7cdaa6ddcc5fefc07c3036d45ebdd4e64029e
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-05-12 01:03:14 +0400

    fixed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,17 @@
 import styles from './style.module.scss';
 
 export const Footer = () => {
+    const openInNewTab = (url: string) => {
+        window.open(url, "_blank", "noreferrer");
+      };
     return (
         <div className={styles.container}>
             <img src="/logos/blocknite.png" alt="" className={styles.logo} />
             <img src="/logos/fantasy.png" alt="" className={styles.fantasy} />
             <div className={styles.socialSites}>
-                <img src="/icons/telegram.svg" alt="telegram" />
-                <img src="/icons/twitter.svg" alt="twitter" />
-                <img src="/icons/youtube.svg" alt="youtube" />
+                <img src="/icons/telegram.svg" alt="telegram" onClick={()=>openInNewTab('https://t.me/Blocknite ')}/>
+                <img src="/icons/twitter.svg" alt="twitter" onClick={()=>openInNewTab('https://twitter.com/blockniteio ')} />
+                <img src="/icons/youtube.svg" alt="youtube" onClick={()=>openInNewTab('https://www.youtube.com/@Blocknite')} />
                 <img src="/icons/Dextools.svg" alt="Dextools" />
                 <img src="/icons/Coingecko.svg" alt="Coingecko" />
                 <img src="/icons/Radium.svg" alt="Radium" />
@@ -16,7 +19,7 @@ export const Footer = () => {
             <div className={styles.info}>
                 <div className={styles.support}>
                     Contact us:
-                    <a href='href="mailto:support@blocknite.io"'>support@blocknite.io</a>
+                    <a href="mailto:support@blocknite.io">support@blocknite.io</a>
                 </div>
                 <div>Copyright © 2024 Blocknite. All rights reserved.</div>
             </div>
