38073d260d38d6b686597289337542154241eb33
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-05-25 01:02:07 +0400

    fixewd

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -47,12 +47,12 @@ export const Screen = ({ id, component, imageUrl, mobileImage, screen }: any) =>
       </div>
     }
 
-    {/* <div style={{
+    <div style={{
       width: '100%',
       height: '100%',
       overflow: 'hidden'
     }}>
       {component}
-    </div> */}
+    </div>
   </div>;
 };
