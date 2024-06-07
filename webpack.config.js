1af3ecc33ea8f9466388fc0b05435d3b9894400a
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-06-08 02:11:24 +0400

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-devServer: {
-    setupMiddlewares: (middlewares, devServer) => {
-      // Your custom middleware code for onAfterSetupMiddleware
-      middlewares.push((req, res, next) => {
-        // Custom middleware functionality
-        next();
-      });
-  
-      // Your custom middleware code for onBeforeSetupMiddleware
-      middlewares.unshift((req, res, next) => {
-        // Custom middleware functionality
-        next();
-      });
-  
-      return middlewares;
+module.exports = {
+    devServer: {
+        setupMiddlewares: (middlewares, devServer) => {
+            // Your custom middleware code for onAfterSetupMiddleware
+            middlewares.push((req, res, next) => {
+                // Custom middleware functionality
+                next();
+            });
+
+            // Your custom middleware code for onBeforeSetupMiddleware
+            middlewares.unshift((req, res, next) => {
+                // Custom middleware functionality
+                next();
+            });
+
+            return middlewares;
+        }
     }
-  }
-  
+}
