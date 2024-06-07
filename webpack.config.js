40b146936b2d5e69b34800c2fa713b1268d11682
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-06-08 02:14:53 +0400

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
+const path = require('path');
+
 module.exports = {
-    devServer: {
-        setupMiddlewares: (middlewares, devServer) => {
-            // Your custom middleware code for onAfterSetupMiddleware
-            middlewares.push((req, res, next) => {
-                // Custom middleware functionality
-                next();
-            });
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    setupMiddlewares: (middlewares, devServer) => {
+      // Custom middleware for onBeforeSetupMiddleware
+      middlewares.unshift((req, res, next) => {
+        // Custom middleware functionality
+        next();
+      });
 
-            // Your custom middleware code for onBeforeSetupMiddleware
-            middlewares.unshift((req, res, next) => {
-                // Custom middleware functionality
-                next();
-            });
+      // Custom middleware for onAfterSetupMiddleware
+      middlewares.push((req, res, next) => {
+        // Custom middleware functionality
+        next();
+      });
 
-            return middlewares;
-        }
-    }
-}
+      return middlewares;
+    },
+  },
+};
