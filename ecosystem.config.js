92c52b306c8f1e46290457495eb30cea75c19a3c
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-06-10 00:27:01 +0400

    push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,21 +8,5 @@ module.exports = {
                 NODE_ENV: 'production',
             }
         }
-    ],
-    deploy: {
-        production: {
-            key: "./davit.pem",
-            user: "blocbdzb",
-            host: ["198.177.120.53"],
-            port: '21098',
-            ref: "origin/main",
-            repo: "https://davit-khachatryan1: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@github.com/davit-khachatryan1/Blocknite.git",
-            path: "/home/blocbdzb/public_html",
-            "pre-setup": "echo 'commands or local script path to be run on the host before the setup process starts'",
-            "post-setup": "echo 'commands or a script path to be run on the host after cloning the repo'",
-            "pre-deploy": "pm2 startOrRestart ecosystem.config.js --env production",
-            "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production",
-            "pre-deploy-local": "echo 'This is a local executed command'"
-        }
-    }
+    ]
 };
